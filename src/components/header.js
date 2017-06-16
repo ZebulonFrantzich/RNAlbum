bfafe2f5f0c51e237d5655df3b2bcde064478de7
Zebulon Frantzich <zeb.frantzich@agapered.com>
2017-06-16 18:46:21 -0500

    Adds unstyled header text to view.

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,7 @@ import { Text } from 'react-native';
 
 // Make Component
 const Header = () => {
-  return <Text>Albums</Text>;
+  return <Text>Albums!</Text>;
 };
 
 // Make component available to other parts of the app
