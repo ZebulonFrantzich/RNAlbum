2ed01b810b89aad73ef48817f88e48b7b86faa02
Zebulon Frantzich <zeb.frantzich@agapered.com>
2017-06-16 18:34:47 -0500

    Initial commit

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+// Import libraries for making Component
+import React from 'react';
+import { Text } from 'react-native';
+
+// Make Component
+const Header = () => {
+  return <Text>Albums</Text>;
+};
+
+// Make component available to other parts of the app
+export default Header;
